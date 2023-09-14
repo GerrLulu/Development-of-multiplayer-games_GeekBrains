a078c02a9333650561da33358ee85124fd9b2b8a
Artyom Dyomin <88819724+GerrLulu@users.noreply.github.com>
2023-09-14 17:55:09 +0300

    HomeWorkLesson5

--- DevelopmentOfMultiplayerGames/Assets/Lesson45/Scripts/PlayFabAccountManager.cs
+++ DevelopmentOfMultiplayerGames/Assets/Lesson45/Scripts/PlayFabAccountManager.cs
@@ -1,6 +1,7 @@
 ﻿using PlayFab;
 using PlayFab.ClientModels;
 using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
@@ -9,6 +10,9 @@ namespace Lesson4
     public class PlayFabAccountManager : MonoBehaviour
     {
         [SerializeField] private TMP_Text _titleLabel;
+        [SerializeField] private Transform _transform;
+
+        private TMP_Text _textInfoCatalog;
 
         private bool _isLoading = true;
 
@@ -17,14 +21,16 @@ namespace Lesson4
         {
             StartCoroutine(Load());
             PlayFabClientAPI.GetAccountInfo(new GetAccountInfoRequest(), OnGetAccount, OnError);
+
+            PlayFabClientAPI.GetCatalogItems(new GetCatalogItemsRequest(), OnGetCatalogSuccess, OnError);
         }
+
 
         private void OnGetAccount(GetAccountInfoResult result)
         {
             _isLoading = false;
             _titleLabel.text = $"Playfab id: {result.AccountInfo.PlayFabId}\n" +
-                $"Playfab name: {result.AccountInfo.Username}\n" +
-                $"ServerCustomIdInfo info: {result.AccountInfo.ServerCustomIdInfo}";
+                $"Playfab name: {result.AccountInfo.Username}";
         }
 
         private void OnError(PlayFabError error)
@@ -43,6 +49,24 @@ namespace Lesson4
                 yield return new WaitForSeconds(0.1f);
                 _titleLabel.text = "Loading ...";
                 yield return new WaitForSeconds(0.1f);
+            }
+        }
+
+
+        private void OnGetCatalogSuccess(GetCatalogItemsResult result)
+        {
+            Debug.Log("OnGetCatalogSuccess");
+            ShowItems(result.Catalog);
+        }
+
+        private void ShowItems(List<CatalogItem> catalog)
+        {
+            foreach (CatalogItem item in catalog)
+            {
+                Instantiate(_titleLabel, _transform);
+                _textInfoCatalog.text = item.DisplayName;
+
+                Debug.Log($"{item.ItemId}");
             }
         }
     }
