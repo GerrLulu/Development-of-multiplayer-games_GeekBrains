ebe03ee090029f5cfb333c0c5cf4526cedab4b28
Artyom Dyomin <88819724+GerrLulu@users.noreply.github.com>
2023-09-26 18:23:31 +0300

    Lesson8

--- DevelopmentOfMultiplayerGames/Assets/Lesson4And5/Scripts/PlayFabAccountManager.cs
+++ DevelopmentOfMultiplayerGames/Assets/Lesson4And5/Scripts/PlayFabAccountManager.cs
@@ -1,9 +1,14 @@
-﻿using PlayFab;
+﻿using Lesson8;
+using PlayFab;
 using PlayFab.ClientModels;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using static UnityEditor.Progress;
 
 namespace Lesson4
 {
@@ -11,20 +16,111 @@ namespace Lesson4
     {
         [SerializeField] private TMP_Text _titleLabel;
         [SerializeField] private Transform _transform;
+        [SerializeField] private GameObject _newCharacterCreatePanel;
+        [SerializeField] private Button _createCharacterButton;
+        [SerializeField] private TMP_InputField _inputField;
+        [SerializeField] private List<SlotCharacterWidget> _slots;
 
         private TMP_Text _textInfoCatalog;
-
         private bool _isLoading = true;
+        private string _characterName;
 
 
         private void Start ()
         {
             StartCoroutine(Load());
             PlayFabClientAPI.GetAccountInfo(new GetAccountInfoRequest(), OnGetAccount, OnError);
-
             PlayFabClientAPI.GetCatalogItems(new GetCatalogItemsRequest(), OnGetCatalogSuccess, OnError);
+
+            GetCharacters();
+
+            foreach (SlotCharacterWidget slot in _slots)
+                slot.SlotButton.onClick.AddListener(OpenCreateNewCharacter);
+
+            _inputField.onValueChanged.AddListener(OnNameChanged);
+            _createCharacterButton.onClick.AddListener(CreateCharacter);
         }
 
+        private void CreateCharacter()
+        {
+            PlayFabClientAPI.GrantCharacterToUser(new GrantCharacterToUserRequest
+            {
+                CharacterName = _characterName,
+                ItemId = "character_token"
+            }, result =>
+            {
+                UpdateCharacterStatistics(result.CharacterId);
+            }, OnError);
+        }
+
+        private void UpdateCharacterStatistics(string characterId)
+        {
+            PlayFabClientAPI.UpdateCharacterStatistics(new UpdateCharacterStatisticsRequest
+            {
+                CharacterId = characterId,
+                CharacterStatistics = new Dictionary<string, int>
+                {
+                    {"Level", 1},
+                    {"Gold  ", 0}
+                }
+            }, result =>
+            {
+                Debug.Log($"Complete");
+                CloseCreateNewCharacter();
+                GetCharacters();
+            }, OnError);
+        }
+
+        private void OnNameChanged(string changedName)
+        {
+            _characterName = changedName;
+        }
+
+        private void OpenCreateNewCharacter()
+        {
+            _newCharacterCreatePanel.SetActive(true);
+        }
+
+        private void CloseCreateNewCharacter()
+        {
+            _newCharacterCreatePanel.SetActive(false);
+        }
+
+        private void GetCharacters()
+        {
+            PlayFabClientAPI.GetAllUsersCharacters(new ListUsersCharactersRequest(),
+                result =>
+                {
+                    Debug.Log($"Characters owned: + {result.Characters.Count}");
+                    ShowCharactersInSlot(result.Characters);
+                }, OnError);
+        }
+
+        private void ShowCharactersInSlot(List<CharacterResult> characters)
+        {
+            if(characters.Count == 0)
+            {
+                foreach(var slot in _slots)
+                    slot.ShowEptySlot();
+            }
+            else if(characters.Count > 0 && characters.Count < _slots.Count)
+            {
+                PlayFabClientAPI.GetCharacterStatistics(new GetCharacterStatisticsRequest
+                {
+                    CharacterId = characters.First().CharacterId
+                }, result =>
+                {
+                    var level = result.CharacterStatistics["Level"].ToString();
+                    var gold = result.CharacterStatistics["Gold"].ToString();
+
+                    _slots.First().ShowInfoCharacter(characters.First().CharacterName, level, gold);
+                }, OnError); ;
+            }
+            else
+            {
+                Debug.Log("Add slots");
+            }
+        }
 
         private void OnGetAccount(GetAccountInfoResult result)
         {
---
+++ DevelopmentOfMultiplayerGames/Assets/Lesson4And5/Scripts/SlotCharacterWidget.cs
@@ -0,0 +1,35 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Lesson8
+{
+    public class SlotCharacterWidget : MonoBehaviour
+    {
+        [SerializeField] private Button _button;
+        [SerializeField] private GameObject _emptySlot;
+        [SerializeField] private GameObject _infoCharacterSlot;
+        [SerializeField] private TMP_Text _nameLabel;
+        [SerializeField] private TMP_Text _levelLabel;
+        [SerializeField] private TMP_Text _goldLabel;
+
+        public Button SlotButton => _button;
+
+
+        public void ShowInfoCharacter(string name, string level, string gold)
+        {
+            _nameLabel.text = name;
+            _levelLabel.text = level;
+            _goldLabel.text = gold;
+
+            _infoCharacterSlot.SetActive(true);
+            _emptySlot.SetActive(false);
+        }
+
+        public void ShowEptySlot()
+        {
+            _infoCharacterSlot.SetActive(false);
+            _emptySlot.SetActive(true);
+        }
+    }
+}
